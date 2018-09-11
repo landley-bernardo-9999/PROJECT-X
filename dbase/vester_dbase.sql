ec769c3911c1f9f44f717941fddad48e1b23a7ac
Landley Bernardo <bernardo.landley.m@gmail.com>
2018-09-11 10:10:53 -0400

    edited database.

--- dbase/vester_dbase.sql
+++ dbase/vester_dbase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 10, 2018 at 06:31 AM
+-- Generation Time: Sep 11, 2018 at 02:06 PM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -25,31 +25,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contactpersons`
+--
+
+DROP TABLE IF EXISTS `contactpersons`;
+CREATE TABLE IF NOT EXISTS `contactpersons` (
+  `contactPersonId` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `rel` varchar(255) DEFAULT NULL,
+  `mobileNumber` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`contactPersonId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `employees`
 --
 
 DROP TABLE IF EXISTS `employees`;
 CREATE TABLE IF NOT EXISTS `employees` (
   `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
   `firstName` varchar(255) DEFAULT NULL,
   `lastName` varchar(255) DEFAULT NULL,
   `birthDate` date DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `pass` varchar(255) DEFAULT NULL,
-  `repeatPass` varchar(255) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
   `zip` varchar(255) DEFAULT NULL,
-  `mobileNumber` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`empid`, `firstName`, `lastName`, `birthDate`, `username`, `pass`, `repeatPass`, `city`, `country`, `zip`, `mobileNumber`) VALUES
-(1, 'Landley', 'Bernardo', '1996-12-03', 'landley_123', 'aaaaaaaaa', 'aaaaaaaa', 'Baguio City', 'Philippines', '2600', ' 9752826318');
+INSERT INTO `employees` (`empid`, `userid`, `firstName`, `lastName`, `birthDate`, `position`, `city`, `country`, `zip`) VALUES
+(1, 0, 'Landley', 'Bernardo', '1996-12-03', NULL, 'Baguio City', 'Philippines', '2600'),
+(2, 0, 'Landley', 'Bernardo', '1996-12-03', NULL, 'Baguio City', 'Philippines', '2600');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owners`
+--
+
+DROP TABLE IF EXISTS `owners`;
+CREATE TABLE IF NOT EXISTS `owners` (
+  `ownerid` int(11) NOT NULL AUTO_INCREMENT,
+  `roomNo` int(11) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `birthDate` date DEFAULT NULL,
+  `mobileNumber` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `zip` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ownerid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -60,7 +95,7 @@ INSERT INTO `employees` (`empid`, `firstName`, `lastName`, `birthDate`, `usernam
 DROP TABLE IF EXISTS `residents`;
 CREATE TABLE IF NOT EXISTS `residents` (
   `resid` int(11) NOT NULL AUTO_INCREMENT,
-  `unitNo` varchar(255) DEFAULT NULL,
+  `roomNo` varchar(255) DEFAULT NULL,
   `bedNo` varchar(255) DEFAULT NULL,
   `stat` varchar(255) DEFAULT NULL,
   `firstName` varchar(255) DEFAULT NULL,
@@ -75,18 +110,51 @@ CREATE TABLE IF NOT EXISTS `residents` (
   `city` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
   `zip` varchar(255) DEFAULT NULL,
-  `contactPerson` varchar(255) DEFAULT NULL,
-  `rel` varchar(255) DEFAULT NULL,
-  `contactPersonMobileNumber` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`resid`)
+  `contactPersonId` int(11) NOT NULL,
+  PRIMARY KEY (`resid`),
+  KEY `contactPersonId` (`contactPersonId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `residents`
 --
 
-INSERT INTO `residents` (`resid`, `unitNo`, `bedNo`, `stat`, `firstName`, `lastName`, `gender`, `school`, `course`, `yearLevel`, `birthDate`, `mobileNumber`, `emailAddress`, `city`, `country`, `zip`, `contactPerson`, `rel`, `contactPersonMobileNumber`) VALUES
-(1, '101', '1', 'Active', 'Landley', 'Bernardo', 'Male', 'Saint Louis University', 'Masters in Information Technology', 1, '1996-12-03', '9752826318', 'lmbernardo@slu.edu.ph', 'Baguio City', 'Philippines', '2600', 'Laurence', 'Sibling', '09098099485345');
+INSERT INTO `residents` (`resid`, `roomNo`, `bedNo`, `stat`, `firstName`, `lastName`, `gender`, `school`, `course`, `yearLevel`, `birthDate`, `mobileNumber`, `emailAddress`, `city`, `country`, `zip`, `contactPersonId`) VALUES
+(1, '101', '1', 'Active', 'Landley', 'Bernardo', 'Male', 'Saint Louis University', 'Masters in Information Technology', 1, '1996-12-03', '9752826318', 'lmbernardo@slu.edu.ph', 'Baguio City', 'Philippines', '2600', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `roomId` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerid` int(11) NOT NULL,
+  `resid` int(11) NOT NULL,
+  PRIMARY KEY (`roomId`),
+  KEY `ownerid` (`ownerid`),
+  KEY `resid` (`resid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `empid` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `emailAddress` varchar(255) DEFAULT NULL,
+  `pass` varchar(255) DEFAULT NULL,
+  `mobileNumber` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  KEY `empid` (`empid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
