1affca246c252dd134a2c36c2db404c96cb8da60
Landley Bernardo <bernardo.landley.m@gmail.com>
2018-09-16 09:14:26 -0400

    redo the fucking database.

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
     define('db_server', 'localhost');
     define('db_username', 'root');
     define('db_password', 'VesterCorporation_123');
-    define('db_name','vester_dbase_2');
+    define('db_name','vester_dbase');
 
     $conn = mysqli_connect(db_server, db_username, db_password, db_name);
 
